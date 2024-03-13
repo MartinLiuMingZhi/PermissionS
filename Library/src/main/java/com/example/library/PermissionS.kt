7c3d0b90ca4131e208a4a41f618d330169c55e79
lenovo <3108531642@qq.com>
2024-03-13 11:05:46 +0800

    完成PermissionS库的开发与测试工作。

--- Library/src/main/java/com/example/library/PermissionS.kt
+++ Library/src/main/java/com/example/library/PermissionS.kt
@@ -6,7 +6,16 @@ object PermissionS {
 
     private const val TAG = "InvisibleFragment"
 
-    fun request(activity: FragmentActivity,vararg permission: String,callback:PermissionCallback) {
-
+    fun request(activity: FragmentActivity,vararg permissions: String,callback:PermissionCallback) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null){
+            existedFragment as InvisibleFragment
+        }else{
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment,TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback,*permissions)
     }
 }
--- app/src/main/java/com/example/permissions/MainActivity.kt
+++ app/src/main/java/com/example/permissions/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.permissions
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.library.PermissionS
+import com.example.permissions.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.makeCallBtn.setOnClickListener {
+            PermissionS.request(this,
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.READ_CONTACTS){ allGranted, deniedList ->
+                    if (allGranted){
+                        call()
+                    }else{
+                        Toast.makeText(this,"You denied $deniedList",Toast.LENGTH_SHORT).show()
+                    }
+
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        }catch (e:SecurityException){
+            e.printStackTrace()
         }
     }
 }
